46ad01ce63fbe5218db16b82b641fb56fdae0407
arkhon7 <reviuy9@gmail.com>
2023-04-26 17:00:20 +0800

    improved ui

--- main.py
+++ main.py
@@ -62,12 +62,13 @@ def generate_predictions(to_pred: pd.DataFrame):
 
 
 def main():
-    st.set_page_config(page_title="ORCA", page_icon="./assets/orca.png")
+    st.set_page_config(page_title="ORCA", page_icon="./assets/orca.png", layout="wide")
 
-    st.write(
+    # sidebar
+    st.sidebar.write("# O.R.C.A Model")
+
+    st.sidebar.write(
         """
-    # O.R.C.A Model
-
     Welcome to O.R.C.A Model playground! It is a machine learning model trained with
     [PAGASA](https://www.pagasa.dost.gov.ph/) and [NAMRIA](https://www.namria.gov.ph/) 
     datasets from 2021. This model is used for forecasting weather at coastal areas in
@@ -78,61 +79,70 @@ def main():
 
     To use the model, just simply put the starting date and the span of days you want
     to forecast.
+    """
+    )
 
+    starting_date = t.cast(date, st.sidebar.date_input(label="starting date"))
+    span = st.sidebar.number_input(label="span of days", min_value=1)
+
+    st.sidebar.write(
+        """
     **Important Note**: having span of days more than a week and a starting date further
     beyond **2022/01/01** will lead to significantly innacurate results.
     """
     )
 
-    inp_col1, inp_col2 = st.columns(2)
-
-    starting_date = t.cast(date, inp_col1.date_input(label="starting date"))
-    span = inp_col2.number_input(label="span of days", min_value=1)
-
     dates = generate_dates(from_=t.cast(date, starting_date), to_=int(span))
     preds = generate_predictions(dates)
 
+    cont1 = st.container()
+    col1, col2 = cont1.columns(2)
+
     st.divider()
-    st.write(
-        f"""
-        Mean Water Level from `{starting_date}` to 
-        `{starting_date + timedelta(span)}` measured in centimeters (cm)
-        """
-    )
-    st.line_chart(preds["mean water level"])
-    st.divider()
-    st.write(
-        f"""
-        Mean Sea Level Pressure from `{starting_date}` to 
-        `{starting_date + timedelta(span)}` measured in millibars (mbar)
-        """
-    )
-    st.line_chart(preds["mean sea level pressure"])
-    st.divider()
-    st.write(
-        f"""
-       Temperature from `{starting_date}` to 
-        `{starting_date + timedelta(span)}` measured in degrees celsius
-        """
-    )
-    st.line_chart(preds[["max temperature", "min temperature", "mean temperature"]])
-    st.divider()
-    st.write(
-        f"""
-        Mean Relative Humidity from `{starting_date}` to 
-        `{starting_date + timedelta(span)}` measured in percent
-        """
-    )
-    st.line_chart(preds["mean relative humidity"])
-    st.divider()
-    st.write(
-        f"""
-        Mean Wind Speed from `{starting_date}` to 
-        `{starting_date + timedelta(span)}` measured in meters per second
-        """
-    )
-    st.line_chart(preds["mean wind speed"])
-    st.divider()
+
+    with col1:
+        st.write(
+            f"""
+            Mean Water Level from `{starting_date}` to 
+            `{starting_date + timedelta(span)}` measured in centimeters (cm)
+            """
+        )
+        st.line_chart(preds["mean water level"])
+        st.divider()
+
+        st.write(
+            f"""
+            Mean Sea Level Pressure from `{starting_date}` to 
+            `{starting_date + timedelta(span)}` measured in millibars (mbar)
+            """
+        )
+        st.line_chart(preds["mean sea level pressure"])
+
+        st.divider()
+        st.write(
+            f"""
+            Mean Wind Speed from `{starting_date}` to 
+            `{starting_date + timedelta(span)}` measured in meters per second
+            """
+        )
+        st.line_chart(preds["mean wind speed"])
+
+    with col2:
+        st.write(
+            f"""
+        Temperature from `{starting_date}` to 
+            `{starting_date + timedelta(span)}` measured in degrees celsius
+            """
+        )
+        st.line_chart(preds[["max temperature", "min temperature", "mean temperature"]])
+        st.divider()
+        st.write(
+            f"""
+            Mean Relative Humidity from `{starting_date}` to 
+            `{starting_date + timedelta(span)}` measured in percent
+            """
+        )
+        st.line_chart(preds["mean relative humidity"])
 
 
 if __name__ == "__main__":
